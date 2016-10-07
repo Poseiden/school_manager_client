d5931ba77009f545553a767d8f63209f2e0db171
HaiboZhu <hbzhu@thoughtworks.com>
2016-10-07 11:02:26 +0800

    first blood

---
+++ src/components/Main.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export class AppComponent extends React.Component {
+
+  constructor() {
+
+    super();
+
+    this.doLogin = this.doLogin.bind(this);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  doLogin() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.username} type="text" className="username-input" onChange={(event) => {
+          this.setState({
+            username: event.target.value
+          });
+        }} placeholder="Username" />
+      <br />
+        <input value={this.state.password} type="password" className="password-input" onChange={(event) => {
+          this.setState({
+            password: event.target.value
+          });
+        }} placeholder="Password" />
+        <br />
+        <input type="button" className="submit" value="Login" onClick={this.doLogin} />
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
---
+++ test/components/MainTest.js
@@ -0,0 +1,110 @@
+/*eslint-env node, mocha */
+/*global expect */
+/*eslint no-console: 0*/
+'use strict';
+
+import React from 'react';
+import { mount } from 'enzyme';
+import { assert } from 'chai';
+import Main from 'components/Main';
+
+describe('MainComponent', () => {
+  let MainComponent;
+
+  beforeEach(() => {
+    MainComponent = mount(<Main />);
+  });
+
+  describe('render', () => {
+    it('should has TextInput', () => {
+      const input = MainComponent.find('.username-input');
+
+      assert.equal(input.length, 1);
+      assert.equal(input.prop('placeholder'), 'Username');
+      assert.equal(input.prop('type'), 'text');
+    });
+
+    it('should has PasswordInout', () => {
+      const input = MainComponent.find('.password-input');
+
+      assert.equal(input.length, 1);
+      assert.equal(input.prop('placeholder'), 'Password');
+      assert.equal(input.prop('type'), 'password');
+    });
+
+    it('should has Button', () => {
+      const button = MainComponent.find('.submit');
+
+      assert.equal(button.length, 1);
+      assert.equal(button.prop('value'), 'Login');
+      assert.equal(button.prop('type'), 'button');
+    });
+
+    it('should pass doLogin to Button as onClick event handler', () => {
+      const button = MainComponent.find('.submit');
+
+      assert.equal(button.prop('onClick').name, 'doLogin');
+    });
+
+    it('should has state for save username', () => {
+      assert.equal(MainComponent.state().username, '');
+    });
+
+    it('should has state for save password', () => {
+      assert.equal(MainComponent.state().password, '');
+    });
+
+    it('should change state.username when username changed', () => {
+      const input = MainComponent.find('.username-input');
+
+      assert.equal(MainComponent.state().username, '');
+
+      assert.equal(typeof input.prop('onChange'), 'function');
+
+      input.prop('onChange')({
+        target: {
+          value: 'username'
+        }
+      });
+
+      assert.equal(MainComponent.state().username, 'username');
+    });
+
+    it('should change state.password when password changed', () => {
+      const input = MainComponent.find('.password-input');
+
+      assert.equal(MainComponent.state().password, '');
+
+      assert.equal(typeof input.prop('onChange'), 'function');
+
+      input.prop('onChange')({
+        target: {
+          value: 'password'
+        }
+      });
+
+      assert.equal(MainComponent.state().password, 'password');
+    });
+  });
+
+  describe('doLogin', () => {
+    it('should call function validate with username and password do validation', () => {
+      // const validate = sinon.stub().returns(true);
+
+      // console.log(validate);
+    });
+
+    it('should redirect to home page when validate is true', () => {
+
+    });
+
+    it('should alert error message when validate is false', () => {
+
+    });
+  });
+
+  describe('validate', () => {
+
+  });
+
+});
