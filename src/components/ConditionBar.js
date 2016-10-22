e8e6241ee2ad60b2aff625d8e0938e32436fc925
HaiboZhu <hbzhu@thoughtworks.com>
2016-10-22 18:37:20 +0800

    [hbzhu] modi: change value of 'all' option in gender select.

--- src/components/ConditionBar.js
+++ src/components/ConditionBar.js
@@ -19,22 +19,25 @@ class ConditionBar extends React.Component {
             name : event.target.value
           });
         }}/>
+        <br />
         MaxAge: <input type="text" className="maxAge-input" onChange={(event) => {
           this.setState({
             maxAge: event.target.value
           })
         }} />
+        ~
         MinAge: <input type="text" className="minAge-input" onChange={(event) => {
           this.setState({
             minAge: event.target.value
           })
         }} />
+        <br />
         Gender: <select className="gender-input" onChange={(event) => {
             this.setState({
                 gender: event.target.value
             })
         }}>
-          <option value="all" >all</option>
+          <option value="all" >All</option>
           <option value="male" >Male</option>
           <option value="female" >Female</option>
         </select>
